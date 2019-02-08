22a92d42818544fbe15ae7d485e485fbc73d1684
rahul078 <33972037+rahul078@users.noreply.github.com>
2019-02-08 08:25:58 +0530

    Add files via upload
    register.php added file exists error

--- register.php
+++ register.php
@@ -18,6 +18,19 @@ $proj=$_POST["proj_name"];
 //echo $target_path;
 //echo "$name";
 //echo "$college , $phone , $email";
+if (file_exists($target_path)) {?>
+  <div class="jumbotron text-xs-center">
+    <h1 class="display-3">Sorry , Couldnot register</h1>
+    <p class="lead"><strong>Note</strong> File name should be same as project name provided</p>
+
+  <hr>
+
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="index.html" role="button">Continue to homepage</a>
+  </p>
+</div><?php
+exit();
+}
 
 $conn = mysqli_connect("localhost", "Techno_organizers", "SecuredPwd","Applied_candidates");
 if (!$conn) {
@@ -29,8 +42,8 @@ $sql = "INSERT INTO candidate_list (project_name,name, email, phone,file_url,col
 if (mysqli_query($conn, $sql)) {
   if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {?>
     <div class="jumbotron text-xs-center">
-    <h1 class="display-3">Congrats on registering to the event!</h1>
-    <p class="lead"><strong>Scrutinzed names</strong> will be announced soon.</p>
+      <h1 class="display-3">Congrats on registering to the event!</h1>
+      <p class="lead"><strong>Scrutinzed names</strong> will be announced soon.</p>
     <hr>
 
     <p class="lead">
